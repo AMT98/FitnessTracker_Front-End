e2475d8c6137cc398788cc0567e937e5bad5f3cb
AMT98 <aswinmalla12@gmail.com>
2023-02-09 15:30:56 -0700

    install packaged and render out app.js

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      {/* navBar */}
+      <div>
+        <h1 className="text-4xl">Hello, World!</h1>
+      </div>
+      {/* footer */}
+    </div>
+  );
+};
+
+export default App;
