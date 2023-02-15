7292c03d25265e6af520d6f1f62c83e72f9a432a
AMT98 <aswinmalla12@gmail.com>
2023-02-15 16:09:15 -0700

    Add styles to home and about page

--- src/components/About.js
+++ src/components/About.js
@@ -2,45 +2,52 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="flex md:flex-row flex-col">
-      <div className="w-full h-screen bg-[#222222]  p-4 text-[#E3FFA8] flex flex-col gap-8 items-center">
-        <h1 className="text-7xl">About GitFit</h1>
+    <div className="bg-[#222222] flex md:flex-col flex-row w-screen h-screen">
+      <div className="w-[50%] ml-[25%] p-4 text-[#E3FFA8] flex flex-col gap-8 items-center">
+        <h1 className="text-7xl text-[#6ED8B4] font-bold ">About GitFit</h1>
         <h1 className="text-sm md:text-lg">
           Gitfit is a web app designed to help you stay active and healthy by
           providing a variety of workout routines and activities that fit your
           lifestyle. Whether you're a busy professional looking for a quick
           workout, or a fitness enthusiast seeking to mix up your routine,
-          Gitfit has something for everyone. Our platform features an extensive
-          library of workout routines and activities that can be tailored to
-          your fitness level and preferences. Our routines range from bodyweight
-          exercises and yoga to strength training and cardio workouts. Each
-          routine is designed to provide a full-body workout and improve your
-          overall health and fitness. In addition to our pre-designed routines,
-          we also offer routines with activities that allow you to incorporate
-          your favorite hobbies and interests into your workout. Whether you
-          enjoy hiking, swimming, or dancing, we have a routine that will help
-          you stay active while doing something you love. At Gitfit, we believe
-          that staying active and healthy should be an enjoyable and rewarding
-          experience. Our platform is user-friendly and easy to navigate, and
-          our routines and activities are designed to keep you motivated and
-          engaged. With Gitfit, you can stay on track with your fitness goals
-          and lead a healthier, happier life. Thank you for considering Gitfit
-          as your go-to fitness app, and we look forward to helping you achieve
-          your fitness goals.
+          Gitfit has something for everyone.
+        </h1>
+        <h1 className="text-sm md:text-lg">
+          Our platform features an extensive library of workout routines and
+          activities that can be tailored to your fitness level and preferences.
+          Our routines range from bodyweight exercises and yoga to strength
+          training and cardio workouts. Each routine is designed to provide a
+          full-body workout and improve your overall health and fitness. In
+          addition to our pre-designed routines, we also offer routines with
+          activities that allow you to incorporate your favorite hobbies and
+          interests into your workout. Whether you enjoy hiking, swimming, or
+          dancing, we have a routine that will help you stay active while doing
+          something you love.
+        </h1>
+        <h1 className="text-sm md:text-lg">
+          At Gitfit, we believe that staying active and healthy should be an
+          enjoyable and rewarding experience. Our platform is user-friendly and
+          easy to navigate, and our routines and activities are designed to keep
+          you motivated and engaged. With Gitfit, you can stay on track with
+          your fitness goals and lead a healthier, happier life. Thank you for
+          considering Gitfit as your go-to fitness app, and we look forward to
+          helping you achieve your fitness goals.
         </h1>
       </div>
       <div
         name="contact"
         className="w-full h-screen bg-[#222222] p-4 text-[#E3FFA8]"
       >
-        <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
+        <div className="flex flex-col p-4  max-w-screen-lg mx-auto h-full">
           <div className=" flex justify-center items-center">
             <form
               action="https://getform.io/f/a012141e-4460-45a9-9393-bb60cc0767c2"
               method="POST"
               className=" flex flex-col w-full md:w-1/2"
             >
-              <p className="text-4xl mb-4  font-bold ">Any questions?</p>
+              <p className="text-4xl mb-4  text-[#6ED8B4] font-bold ">
+                Any questions?
+              </p>
               <h1 className="text-xl mb-4  font-bold ">
                 Please fill out all sections
               </h1>
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,40 +6,38 @@ const Home = () => {
   return (
     <>
       <div className="bg-[#000000] flex flex-col md:flex-row gap-11 h-screen w-screen">
-        <div>
-          <h1 className="text-8xl text-[#6ED8B4] font-bold capitalize">
+        <div className="flex md:ml-[14%] md:w-45% w-6/12 ml-[7%] mt-[4%] flex-col items-center">
+          <h1 className="text-4xl md:text-8xl text-[#6ED8B4] font-bold capitalize mb-[5%]">
             GitFit fitness club
           </h1>
-          <h2 className="text-4xl text-[#E3FFA8] font-bold capitalize">
+          <h2 className="md:text-4xl text-lg text-[#E3FFA8] font-bold capitalize mb-[3%]">
             Ignite Your Inner Fire and Conquer Your Fitness Goals
           </h2>
-          <h4 className="text-[#E3FFA8] text-xl">
+          <h4 className="text-[#E3FFA8] text-md md:text-lg mb-[3%]">
             Are you ready to unleash your inner badass and dominate your fitness
             journey like a true champion? It's time to ignite your inner fire
             and take control of your health and wellbeing with the help of
             GitFit.
+            <br></br>So what are you waiting for? It's time to unleash your
+            inner beast and start taking control of your fitness journey like a
+            true badass. Sign up today and join the revolution of fitness
+            warriors who are transforming their bodies and their lives.
           </h4>
-          <h4 className="text-[#E3FFA8] text-xl">
-            So what are you waiting for? It's time to unleash your inner beast
-            and start taking control of your fitness journey like a true badass.
-            Sign up today and join the revolution of fitness warriors who are
-            transforming their bodies and their lives.
-          </h4>
-          <div className="flex flex-row gap-4 ">
+          <div className="flex flex-row gap-4 items-center">
             <NavLink to="/login">
-              <button className="border p-3 md:p-4 md:px-6 border-[#6ED8B4] hover:border-[#E3FFA8] text-[#E3FFA8] font-bold duration-300 rounded-xl text-xs md:text-md hover:text-[#6ED8B4]">
+              <button className="border p-3 md:p-4 md:px-6 border-[#6ED8B4] hover:border-[#E3FFA8] text-[#E3FFA8] font-bold duration-300 rounded-xl text-xs md:text-lg hover:text-[#6ED8B4]">
                 Sign In
               </button>
             </NavLink>
             <NavLink to="/register">
-              <button className="border p-3 md:p-4 border-[#6ED8B4] font-bold hover:border-[#E3FFA8] text-[#E3FFA8] duration-300 rounded-xl text-xs md:text-md hover:text-[#6ED8B4]">
+              <button className="border p-3 md:p-4 border-[#6ED8B4] font-bold hover:border-[#E3FFA8] text-[#E3FFA8] duration-300 rounded-xl text-xs md:text-lg hover:text-[#6ED8B4]">
                 Become a Member
               </button>
             </NavLink>
           </div>
         </div>
         <div>
-          <img src={heroImg} alt="heroImg" />
+          <img src={heroImg} alt="heroImg" className="hidden md:block" />
         </div>
       </div>
     </>
