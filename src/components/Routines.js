5176a98bc8cf568163c59026d52aa4e11d771e72
Don Nnguyen <donng24@gmail.com>
2023-02-15 21:34:45 -0600

    routines

--- src/components/Routines.js
+++ src/components/Routines.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import {useState, useEffect} from 'react'
+import {fetchPublicRoutines} from "../api/api"
 
 const Routines = () => {
+  const [routines, setRoutines] = useState([]);
+
+  useEffect(() => {
+    const fetchRoutines = async () => {
+      const data = await fetchPublicRoutines();
+      setRoutines(data);
+    };
+    fetchRoutines();
+  }, []);
+
   return (
-    <div>Routines</div>
-  )
-}
+    <div>
+      <h1>Routines</h1>
+      <ul>
+        {routines.map((routine) => (
+          <li key={routine.id}>
+            {routine.name} ({routine.goal})
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Routines
