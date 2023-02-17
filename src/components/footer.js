8cdeca9eae827bc30edeee8407dfa7c2137cefa5
AMT98 <aswinmalla12@gmail.com>
2023-02-17 13:44:34 -0700

    Fixed errors in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,53 +1,49 @@
 import React from "react";
 // import { Link } from "react-scroll";
 import instalogo from "../assets/insta logo.png";
-import twitterlogo from "../assets/twitter.png"
+import twitterlogo from "../assets/twitter.png";
 import { NavLink } from "react-router-dom";
 
-
 const Footer = () => {
-    const links = [
-        {
-            id: 1,
-            link: "Home",
-            path: "/",
-        },
-        {
-            id: 2,
-            link: "Get Started",
-            path: "/register",
-        },
-        {
-            id: 3,
-            link: "Company",
-            path: "/careers",
-        },
-        {
-            id: 4,
-            link: "Policy",
-            path: "/policy",
-        },
-    ];
-    return (<div className="footer">
-        
-        <ul>
-            {links.map(({ id, link, path }) => (
-                <li
-                    key={id}
-                    className="footerlinks"
-                >
-                    <NavLink to={path} smooth duration={500}>
-                        {link}
-                    </NavLink>
-                </li>
-            ))}
-        </ul>
+  const links = [
+    {
+      id: 1,
+      link: "Home",
+      path: "/",
+    },
+    {
+      id: 2,
+      link: "Get Started",
+      path: "/register",
+    },
+    {
+      id: 3,
+      link: "Company",
+      path: "/careers",
+    },
+    {
+      id: 4,
+      link: "Policy",
+      path: "/policy",
+    },
+  ];
+  return (
+    <div className="footer">
+      <ul>
+        {links.map(({ id, link, path }) => (
+          <li key={id} className="footerlinks">
+            <NavLink to={path} duration={500}>
+              {link}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
 
-        <div id="findus">Find Us On Other Social Networks:</div>
-            <img class="social" src={instalogo} alt="instagram logo"></img>
-            <img class="social" src={twitterlogo} alt="instagram logo"></img>
-        </div>)
+      <div id="findus">Find Us On Other Social Networks:</div>
+      <img className="social" src={instalogo} alt="instagram logo"></img>
+      <img className="social" src={twitterlogo} alt="instagram logo"></img>
+    </div>
+  );
+};
 
-}
-
-export default Footer
+export default Footer;
