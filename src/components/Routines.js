d9354959ff7e8e945be55d839065602755c11bdd
Don Nnguyen <donng24@gmail.com>
2023-02-16 20:20:05 -0600

    routine work

--- src/components/Routines.js
+++ src/components/Routines.js
@@ -8,6 +8,20 @@ import { FaHeartbeat } from "react-icons/fa";
 
 const Routines = () => {
   const [routines, setRoutines] = useState([]);
+  const [name, setName] = useState("")
+  const [goal, setGoal] = useState("")
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const newRoutine = await createRoutine({
+      isPublic: true,
+      name: `${name}`,
+      goal: `${goal}`,
+    });
+    setRoutines([...routines, newRoutine]);
+    setName("");
+    setGoal("");
+  };
 
   useEffect(() => {
     const fetchRoutines = async () => {
@@ -22,15 +36,28 @@ const Routines = () => {
       <h1 className="text-[#E3FFA8] text-4xl flex items-center justify-center mb-[2%]">
         Routines
       </h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Goal:
+          <input type="text" value={goal} onChange={(e) => setGoal(e.target.value)} />
+        </label>
+        <br />
+        <button type="submit">Create Routine</button>
+      </form>
       <ul className="flex flex-row h-screen w-screen gap-6 flex-wrap">
         {routines.map((routine) => (
           <li
             key={routine.id}
             className="flex flex-col border rounded-2xl p-6 font-bold border-[#E3FFA8] bg-[#45FFBC] w-[20%] h-[35%] shadow-lg  shadow-[#E3FFA8]"
           >
-            <GiMuscleUp className="ml-[45%] mb-[15%]" size={30}/>
+            <GiMuscleUp className="ml-[45%] mb-[15%]" size={30} />
             <h1 className="uppercase mb-[20px]">
-              {<MdFitnessCenter />} {routine.name}
+              <MdFitnessCenter /> {routine.name}
             </h1>
 
             <FaHeartbeat />
