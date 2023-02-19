7a58857046dcf688e26d0769167c68ffdee0a206
AMT98 <aswinmalla12@gmail.com>
2023-02-18 19:19:16 -0700

    Create editRoutines

--- src/components/MyRoutines.js
+++ src/components/MyRoutines.js
@@ -1,14 +1,16 @@
 import React from "react";
 import AddRoutine from "./AddRoutine";
+import EditRoutine from "./EditRoutine";
 
-const MyRoutines = ({setIsLoading}) => {
+const MyRoutines = ({ setIsLoading }) => {
   return (
     <>
       <div className="bg-black  items-center flex p-6 flex-col h-screen w-full">
         <h1 className="text-[#6ED8B4] text-6xl flex items-center justify-center mt-[17%] md:mt-[7%] mb-[1%] ">
           My Routines
         </h1>
-        <AddRoutine setIsLoading={setIsLoading}/>
+        <AddRoutine setIsLoading={setIsLoading} />
+        <EditRoutine setIsLoading={setIsLoading} />
       </div>
     </>
   );
